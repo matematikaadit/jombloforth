f5fd08a21d0737cc286f7ccf9881b2c02ff7d043
Adit Cahya Ramadhan <matematika.adit@gmail.com>
2019-03-13 12:01:42 +0700

    Exceptions

--- jombloforth.f
+++ jombloforth.f
@@ -633,3 +633,78 @@
 ;
 
 
+( Exception )
+: EXCEPTION-MARKER
+    RDROP
+    0
+;
+
+: CATCH
+    DSP@ 8+ >R
+    ' EXCEPTION-MARKER 8+
+    >R
+    EXECUTE
+;
+
+: THROW
+    ?DUP IF
+        RSP@
+        BEGIN
+            DUP R0 8- <
+        WHILE
+            DUP @
+            ' EXCEPTION-MARKER 8+ = IF
+                8+
+                RSP!
+                DUP DUP DUP
+                R>
+                8-
+                SWAP OVER
+                !
+                DSP! EXIT
+            THEN
+            8+
+        REPEAT
+        DROP
+        CASE
+        0 1- OF ( ABORT )
+            ." ABORTED" CR
+        ENDOF
+            ." UNCAUGHT THROW "
+            DUP . CR
+        ENDCASE
+        QUIT
+    THEN
+;
+
+: ABORT
+    0 1- THROW
+;
+
+: PRINT-STACK-TACE
+    RSP@
+    BEGIN
+        DUP R0 8- <
+    WHILE
+        DUP @
+        CASE
+        ' EXCEPTION-MARKER 8+ OF
+            ." CATCH ( DSP="
+            8+ DUP @ U.
+            ." ) "
+        ENDOF
+            DUP
+            CFA>
+            ?DUP IF
+                2DUP
+                ID.
+                [ CHAR + ] LITERAL EMIT
+                SWAP >DFA 8+ - .
+            THEN
+        ENDCASE
+        8+
+    REPEAT
+    DROP
+    CR
+;
+
